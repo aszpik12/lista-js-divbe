a94b7fdf1c2d093b506beea115cde3b20b1c0ec2
Asztrik <asztrikpetrovai@gmail.com>
2023-03-02 16:57:46 +0100

    div betétele lista

---
+++ lista.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", init);
+function init() {
+  const list = [
+    { nev: "Peti", mondat: "A kedd!" },
+    {
+      nev: "Gergo",
+      mondat: "A kedd masodszor!",
+      szemszin: "Kek/Zold",
+      kor: 23,
+    },
+    { nev: "Niki", mondat: "Délelőtt kint játszottam a kutyussal" },
+    { nev: "Asztrik", mondat: "Kedden, mert ott nem történt semmi." },
+    { nev: "GergőT", mondat: "Kedd a legjóbb nap." },
+    { név: "Sanyi", mondat: "Tegnap sikerült időben megoldani a java feladatot."},
+    { nev: "Márton", mondat: "Nem volt angol hétfőn" },
+    { nev: "Dominik", mondat: "Kedd, mert a szölőben a kistraktorral tevékenykedtem."},
+    { nev: "Marci", mondat: "Ha gyenge vagy akkor legalább ne sírj" },
+    { nev: "Bence", mondat: "Pillanat türelmet kérek javítom a mikrofont." },
+  ];
+}
+
+function elemlet(lista){
+    for {let i = 0; i <}
+}
